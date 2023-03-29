8f58bc9a198030df3cedab46e928af6870b8ea00
nnhi9428 <nnhi9428@gmail.com>
2023-03-29 15:56:02 +0700

    fix: remove log

--- src/services/bannerAPI.js
+++ src/services/bannerAPI.js
@@ -2,6 +2,5 @@ import axios from "axios";
 import { API_URL } from "../constant/url";
 
 export const getHomeBannerAPI = () => {
-  console.log(API_URL.BANNER);
   return axios.get(API_URL.BANNER);
 };
