32b0aed433e3867ef6ef14a034ac762b7afce5cb
nnhi9428 <nnhi9428@gmail.com>
2023-03-29 12:02:41 +0700

    Doing task SEC-04

---
+++ src/components/SkillsGrid.jsx
@@ -0,0 +1,8 @@
+import {Row, Col} from "antd"
+
+const SkillsGrid = () => {
+  return (
+	<Row><Col span={16}></Col></Row>
+	)
+}
+export default SkillsGrid;
--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -9,37 +9,26 @@ import { settings } from "./StorySlideConfig";
 const Story = ({ stories }) => {
   return (
     <>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="50"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-mirror="true"
-        data-aos-once="false"
-        data-aos-anchor-placement="top-center"
-      >
-        <Row>
-          <Col offset={4} span={16} className="story-title">
-            What alumni saying
-          </Col>
-        </Row>
+      <Row>
+        <Col offset={4} span={16} className="story-title">
+          What alumni saying
+        </Col>
+      </Row>
 
-        <Row>
-          <Col
-            span={16}
-            sm={{ span: 20, offset: 2 }}
-            xs={{ span: 22, offset: 1 }}
-            offset={4}
-          >
-            <Slider {...settings}>
-              {stories?.map((story) => (
-                <StoryItem key={story.id} userStory={story} />
-              ))}
-            </Slider>
-          </Col>
-        </Row>
-      </div>
+      <Row>
+        <Col
+          span={16}
+          sm={{ span: 20, offset: 2 }}
+          xs={{ span: 22, offset: 1 }}
+          offset={4}
+        >
+          <Slider {...settings}>
+            {stories?.map((story) => (
+              <StoryItem key={story.id} userStory={story} />
+            ))}
+          </Slider>
+        </Col>
+      </Row>
     </>
   );
 };
