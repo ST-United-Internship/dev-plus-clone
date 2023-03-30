3fca93f5a2bd7206e62e796f6c52372066165e28
nnhi9428 <nnhi9428@gmail.com>
2023-03-30 09:36:25 +0700

    feat: list skills ui

--- src/components/SkillsGrid.jsx
+++ src/components/SkillsGrid.jsx
@@ -1,8 +1,51 @@
-import {Row, Col} from "antd"
+import { Col, Row } from "antd";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "antd/dist/antd.css";
 
-const SkillsGrid = () => {
+import "./css/skillsgrid.css";
+
+function BasicExample() {
+  const [itemData, setItemData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3004/item").then((response) => {
+      const data = response.data;
+      console.log(data);
+      setItemData({ data });
+    });
+  }, []);
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-	<Row><Col span={16}></Col></Row>
-	)
+    <>
+      <Row className="item-container" gutter={30}>
+        {itemData?.data?.map((item) => {
+          console.log(item.id);
+          return (
+            <Col
+              data-aos="fade-up"
+              data-aos-duration={item.id * 100 + 600}
+              className="gutter-row"
+              span={8}
+            >
+              <div className="container-card" style={{ display: "flex" }}>
+                <div className="image">
+                  <img className="image-item" src={item.image} />
+                </div>
+                <div className="text-title">
+                  <h4 className="title">{item.title}</h4>
+                  <span className="courses">{item.courses}</span>
+                </div>
+              </div>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
 }
-export default SkillsGrid;
+export default BasicExample;
