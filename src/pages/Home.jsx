19923211ea61ebd61f74dba7a4c099f07f26a59d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-03-30 14:58:45 +0700

    feat: footer ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import HomeBanner from "../components/HomeBanner";
 import Story from "../components/Story/Story";
 import { useGetBanner } from "../hooks/useBanner";
 import { useGetStories } from "../hooks/useGetStroies";
+import { useGetFooter } from "../hooks/useFooter";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
 import Aos from "aos";
@@ -16,11 +17,13 @@ const Home = () => {
 
   const { data: stories } = useGetStories();
 
+  const { data: footerData, isLoading: loadFooterData } = useGetFooter();
+
   return (
     <>
       <HomeBanner homeBannerData={homeBannerData} />
       <Story stories={stories} />
-      <Footer />
+      {!loadFooterData && <Footer footerData={footerData} />}
     </>
   );
 };
