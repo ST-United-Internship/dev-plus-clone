782d8bcd1edffb42c19b03c88a065304931a62db
demosofa <thientndgcd18746@fpt.edu.vn>
2023-03-30 10:57:26 +0700

    feat: loading ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import HomeBanner from "../components/HomeBanner";
+import { Loading } from "../components/Loading";
 import Story from "../components/Story/Story";
 import { useGetBanner } from "../hooks/useBanner";
 import { useGetStories } from "../hooks/useGetStroies";
-import { useEffect } from "react";
-import "aos/dist/aos.css";
-import Aos from "aos";
 
 const Home = () => {
   useEffect(() => {
     Aos.init({ duration: 1500, easing: "ease", once: true });
   }, []);
 
-  const { data: homeBannerData } = useGetBanner();
+  const { data: homeBannerData, isLoading: loadBanner } = useGetBanner();
 
-  const { data: stories } = useGetStories();
+  const { data: stories, isLoading: loadStories } = useGetStories();
+
+  if (loadStories || loadBanner) return <Loading />;
 
   return (
     <>
