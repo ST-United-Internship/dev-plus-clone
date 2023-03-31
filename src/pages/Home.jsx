4b19b29acfd97f4447761f54ee2cf6dde0d30cf4
demosofa <62540039+demosofa@users.noreply.github.com>
2023-03-31 09:30:55 +0700

    Feat/sec 10 (#16)
    * feat: loading ui
    
    * fix: continue rebase dev

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,24 +8,27 @@ import "aos/dist/aos.css";
 import Aos from "aos";
 import Footer from "../components/Footer";
 import SkillGrid from "../components/SkillsGrid";
+import { Loading } from "../components/Loading";
 
 const Home = () => {
   useEffect(() => {
     Aos.init({ duration: 1500, easing: "ease", once: true });
   }, []);
 
-  const { data: homeBannerData } = useGetBanner();
+  const { data: homeBannerData, isLoading: loadBanner } = useGetBanner();
 
-  const { data: stories } = useGetStories();
+  const { data: stories, isLoading: loadStories } = useGetStories();
 
   const { data: footerData, isLoading: loadFooterData } = useGetFooter();
+
+  if (loadStories || loadBanner || loadFooterData) return <Loading />;
 
   return (
     <>
       <HomeBanner homeBannerData={homeBannerData} />
       <SkillGrid />
       <Story stories={stories} />
-      {!loadFooterData && <Footer footerData={footerData} />}
+      <Footer footerData={footerData} />
     </>
   );
 };
